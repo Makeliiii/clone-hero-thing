f9322016d69c42d77a02cb4e3c2cd03b7992a039
Mäkeli <joel.m.maenpaa@gmail.com>
2020-10-27 13:42:49 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`App listening on port: ${port}`))
