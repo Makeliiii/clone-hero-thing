73696a21e28c09b621c6388f07c67e858ffa79e2
Mäkeli <joel.m.maenpaa@gmail.com>
2020-10-27 15:42:49 +0200

    get by song name to be improved sometime

--- src/routes/api/songs.js
+++ src/routes/api/songs.js
@@ -5,6 +5,7 @@ const router = express.Router()
 import Song from '../../models/Song'
 
 // ROUTE GET /api/songs
+// get all songs in the database
 router.get('/', (req, res) => {
     Song.find()
         .then(songs => {
@@ -19,6 +20,34 @@ router.get('/', (req, res) => {
             return res.status(404).json({
                 error: err,
                 status: 'Songs not found',
+                success: false
+            })
+        })
+})
+
+// ROUTE GET /api/songs/:name
+// get song by song name
+router.get('/:name', (req, res) => {
+    Song.find({ Name: req.params.name })
+        .then(song => {
+            if (!song.length) {
+                return res.status(404).json({
+                    error: 'Couldn\'t find song',
+                    status: 'Song not found',
+                    success: false
+                })
+            }
+
+            return res.status(200).json({
+                song,
+                status: 'Song found',
+                success: true
+            })
+        })
+        .catch(err => {
+            return res.status(404).json({
+                error: err,
+                status: 'Song not found',
                 success: false
             })
         })
