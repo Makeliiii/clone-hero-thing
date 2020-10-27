de722fe63570e75fce6fc2e66e073d042684244d
Mäkeli <joel.m.maenpaa@gmail.com>
2020-10-27 13:50:14 +0200

    server index file and packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
-const port = process.env.PORT || 8000
+dotenv.config()
 
+// env vars
+const port = process.env.PORT || 8000
+const db = process.env.DB || 'mongodb://localhost:27017/clonehero'
+
+// cors
+app.use(cors)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// server and mongodb connection
 app.listen(port, () => console.log(`App listening on port: ${port}`))
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('MongoDB connection.'))
