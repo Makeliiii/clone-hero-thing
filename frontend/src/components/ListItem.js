8f31b3960b3b76c8edcd314edfe251ed4acc07e0
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-03 15:40:29 +0200

    listitem

---
+++ frontend/src/components/ListItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../styles/listItem.module.css'
+
+const ListItem = props => {
+    return (
+        <ul className={styles.listItem}>
+            <li>{props.name}</li>
+            <li>{props.album}</li>
+            <li>{props.artist}</li>
+            <li>{props.genre}</li>
+            <li>{props.charter}</li>
+            <li>{props.year}</li>
+            <li>{!props.lyrics ? 'Yes' : 'No'}</li>
+            <li>{props.songlength}</li>
+        </ul>
+    )
+}
+
+export default ListItem
