c5eac30999770a35d4eadc797ffed84fca9d9a7a
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-03 15:40:44 +0200

    fetch data and display

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // components
 import Search from '../components/Search'
+import ListItem from '../components/ListItem'
 
 const Main = () => {
+    const [charts, setCharts] = useState([])
     const [text, setText] = useState('')
     const [selects] = useState([
         { label: "Name", value: "name" },
@@ -13,12 +15,40 @@ const Main = () => {
         { label: "Charter", value: "charter" },
     ])
 
+    useEffect(() => {
+        fetch('http://localhost:8000/api/charts')
+            .then(res => res.json())
+            .then(data => {
+                setCharts(data.songs)
+            })
+    }, [])
+
     return (
-        <Search
-            selects={selects}
-            setText={setText}
-            text={text}
-        />
+        <div>
+            <Search
+                selects={selects}
+                setText={setText}
+                text={text}
+            />
+            <ul>
+                {charts.map((chart, index) => {
+                    return (
+                        <li key={index}>
+                            <ListItem 
+                                name={chart.Name}
+                                album={chart.Album}
+                                artist={chart.Artist}
+                                genre={chart.Genre}
+                                charter={chart.Charter}
+                                year={chart.Year}
+                                lyrics={chart.lyrics}
+                                songlength={chart.songlength}
+                            />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
