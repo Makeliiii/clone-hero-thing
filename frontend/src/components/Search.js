c4a77b6225b59be5245bf0419d366408398215eb
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-03 17:05:01 +0200

    some prototype search style

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from '../styles/search.module.css'
 
 const Search = props => {
     return (
-        <form>
-            <select onChange={e => console.log(e.target.value)}>
+        <form className={styles.search}>
+            <select className={styles.select} onChange={e => console.log(e.target.value)}>
                 {props.selects.map(({ value, label }) => {
                     return (
                         <option key={value} value={value}>
@@ -11,8 +12,8 @@ const Search = props => {
                         </option>
                     )})}
             </select>
-            <input type="text" value={props.text} onChange={event => props.setText(event.target.value)} />
-            <input type="submit" value="Search" />
+            <input className={styles.input} type="text" value={props.text} onChange={event => props.setText(event.target.value)} />
+            <input className={styles.button} type="submit" value="Search" />
         </form>
     )
 }
