e36a91ed05c9509f95c3310978aa85fdeb1316f4
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-17 08:56:52 +0200

    propa songlength bruv

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -2,6 +2,12 @@ import React from 'react'
 import styles from '../styles/list.module.scss'
 
 const ListItem = props => {
+    const songlength = () => {
+        const minutes = Math.floor(props.songlength / 60000)
+        const seconds = ((props.songlength % 60000) / 1000).toFixed(0)
+        return (seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+    }
+
     return (
         <tr className={styles.listItem}>
             <td>{props.name}</td>
@@ -11,7 +17,7 @@ const ListItem = props => {
             <td>{props.charter}</td>
             <td>{props.year}</td>
             <td>{!props.lyrics ? 'No' : 'Yes'}</td>
-            <td>{props.songlength}</td>
+            <td>{songlength()}</td>
         </tr>
     )
 }
